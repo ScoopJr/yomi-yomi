daa9e3c2965a3ba4a401f801182c2790cf825401
Guy Hennequin <guy.a.hennequin@gmail.com>
2023-05-21 22:12:47 +0900

    Add a input field for wanikani level with rules

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-// Smooth scrolling
 document.addEventListener('DOMContentLoaded', function() {
+    // Smooth scrolling
     const links = document.querySelectorAll('nav a');
     links.forEach(link => {
       link.addEventListener('click', e => {
@@ -11,13 +11,40 @@ document.addEventListener('DOMContentLoaded', function() {
         });
       });
     });
+  
+    // Mobile Menu
+    const menuToggle = document.getElementById('menu-toggle');
+    const menu = document.getElementById('menu');
+  
+    if (menuToggle && menu) {
+      menuToggle.addEventListener('click', function() {
+        menu.classList.toggle('show');
+      });
+    }
+  
+    // Action enter level request
+    const levelInput = document.getElementById('wanikani-level');
+    const confirmButton = document.getElementById('confirm-level');
+    const errorMessage = document.getElementById('error-message');
+  
+    if (confirmButton) {
+      confirmButton.addEventListener('click', function() {
+        const level = parseInt(levelInput.value);
+        if (!isNaN(level) && level >= 1 && level <= 60) {
+          const targetPage = Math.ceil(level / 10);
+          window.location.href = '#page' + targetPage;
+        } else {
+          errorMessage.textContent = 'Please enter a valid WaniKani level between 1 and 60.';
+        }
+      });
+    }
+  
   });
-
-
-  // Mobile Menu
-const menuToggle = document.getElementById('menu-toggle');
-const menu = document.getElementById('menu');
-
-menuToggle.addEventListener('click', function() {
-  menu.classList.toggle('show');
-});
+  
+  function scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
+  
