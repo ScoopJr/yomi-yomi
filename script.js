887ad1ab2dfda34e36e984e8ea0818762a8f8322
Guy Hennequin <guy.a.hennequin@gmail.com>
2023-06-02 00:08:06 +0900

    Resolve hidden summary dropdown issue

--- script.js
+++ script.js
@@ -77,72 +77,97 @@ document.addEventListener('DOMContentLoaded', function() {
   bar.classList.toggle('open');
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function generateLevels() {
-    var levelsData = [
-      // Level 1
-      [
-        { image: 'images/soon.png', alt: 'Story 1', title: 'Story 1', link: 'story1.html' },
-        { image: 'images/soon.png', alt: 'Story 2', title: 'Story 2', link: 'story2.html' },
-        // Add more stories for Level 1 if needed
-      ],
-      // Level 2
-      [
-        { image: 'placeholder3.jpg', alt: 'Story 3', title: 'Story 3', link: 'story3.html' },
-        // Add more stories for Level 2 if needed
-      ],
-      // Add more levels if needed
-    ];
-  
-    var levelLists = document.getElementsByClassName('level-list');
-    for (var i = 0; i < levelLists.length; i++) {
-      var levelList = levelLists[i];
-      var levelStories = levelsData[i];
-  
-      for (var j = 0; j < levelStories.length; j++) {
-        var story = levelStories[j];
-  
-        var storyItem = document.createElement('li');
-        var storyContent = document.createElement('div');
-        var storyImage = document.createElement('img');
-        var storyLink = document.createElement('a');
-        var storyTitle = document.createElement('p');
-  
-        storyImage.src = 'images/' + story.image;
-        storyImage.alt = story.alt;
-  
-        storyLink.href = story.link;
-        storyLink.textContent = story.title;
-  
-        storyTitle.appendChild(storyLink);
-        storyContent.appendChild(storyImage);
-        storyContent.appendChild(storyTitle);
-  
-        storyItem.appendChild(storyContent);
-        levelList.appendChild(storyItem);
-      }
-    }
+// Hidden dropdown and inverted triangle for level pages
+function toggleContent(element) {
+    var content = element.nextElementSibling;
+    content.classList.toggle("hidden");
+    element.classList.toggle("opened");
   }
   
-  // Call the generateLevels function
-  generateLevels();
-  
-
   
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Level data with story information
+var levelsData = [
+    // Levels 1 to 10
+    [
+      { image: 'story1.jpg', title: 'Story 1', link: 'story1.html' },
+      { image: 'story2.jpg', title: 'Story 2', link: 'story2.html' },
+      { image: 'story3.jpg', title: 'Story 3', link: 'story3.html' },
+      { image: 'story4.jpg', title: 'Story 4', link: 'story4.html' },
+      { image: 'story5.jpg', title: 'Story 5', link: 'story5.html' },
+      { image: 'story6.jpg', title: 'Story 6', link: 'story6.html' }
+      // Add more stories for Level 1 if needed
+    ],
+    // Levels 11 to 20
+    [
+      { image: 'story7.jpg', title: 'Story 7', link: 'story7.html' },
+      { image: 'story8.jpg', title: 'Story 8', link: 'story8.html' },
+      { image: 'story9.jpg', title: 'Story 9', link: 'story9.html' },
+      { image: 'story10.jpg', title: 'Story 10', link: 'story10.html' },
+      { image: 'story11.jpg', title: 'Story 11', link: 'story11.html' },
+      { image: 'story12.jpg', title: 'Story 12', link: 'story12.html' }
+      // Add more stories for Level 2 if needed
+    ],
+    // Levels 21 to 30
+    [
+      // Add stories for Level 21 to 30
+    ],
+    // Levels 31 to 40
+    [
+      // Add stories for Level 31 to 40
+    ],
+    // Levels 41 to 50
+    [
+      // Add stories for Level 41 to 50
+    ],
+    // Levels 51 to 60
+    [
+      // Add stories for Level 51 to 60
+    ]
+  ];
+  
+  // Function to generate stories for a specific level
+  function generateLevelStories(level, stories) {
+    var storyList = document.querySelector('.story-list.level' + level);
+  
+    stories.forEach(function(story) {
+      var storyItem = document.createElement('li');
+      var storyLink = document.createElement('a');
+      var storyImage = document.createElement('img');
+      var storyTitle = document.createElement('p');
+  
+      storyLink.href = story.link;
+      storyLink.textContent = story.title;
+  
+      storyImage.src = 'images/' + story.image;
+      storyImage.alt = story.title;
+  
+      storyItem.appendChild(storyImage);
+      storyItem.appendChild(storyTitle);
+      storyTitle.appendChild(storyLink);
+  
+      storyList.appendChild(storyItem);
+    });
+  }
+  
+  // Generate stories for each level
+  for (var i = 0; i < levelsData.length; i++) {
+    generateLevelStories(i + 1, levelsData[i]);
+  }
+
+
+  
