787db9dadb6452860cdc90bb0a1b8322ebb6decf
Guy Hennequin <guy.a.hennequin@gmail.com>
2023-06-11 21:42:02 +0900

    Add function to handle submit button. Functionality needs to be resolved.

--- script.js
+++ script.js
@@ -82,93 +82,38 @@ document.addEventListener('DOMContentLoaded', function() {
     content.classList.toggle("hidden");
     element.classList.toggle("opened");
   }
-  
-  
+ 
+  function redirectToLevel() {
+    // Get the user input
+    var level = parseInt(document.getElementById("wanikani-level").value);
+
+    // Check if the input is within the valid range (1-60)
+    if (level >= 1 && level <= 60) {
+        // Redirect the user to the corresponding level HTML page
+        window.location.href = "levels/level" + level + ".html";
+        
+    } else {
+        // Display an error message or handle invalid input as desired
+        alert("Invalid level entered. Please enter a number between 1 and 60.");
+    }
+}
+
+
+
 
   
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-// Level data with story information
-var levelsData = [
-    // Levels 1 to 10
-    [
-      { image: 'story1.jpg', title: 'Story 1', link: 'story1.html' },
-      { image: 'story2.jpg', title: 'Story 2', link: 'story2.html' },
-      { image: 'story3.jpg', title: 'Story 3', link: 'story3.html' },
-      { image: 'story4.jpg', title: 'Story 4', link: 'story4.html' },
-      { image: 'story5.jpg', title: 'Story 5', link: 'story5.html' },
-      { image: 'story6.jpg', title: 'Story 6', link: 'story6.html' }
-      // Add more stories for Level 1 if needed
-    ],
-    // Levels 11 to 20
-    [
-      { image: 'story7.jpg', title: 'Story 7', link: 'story7.html' },
-      { image: 'story8.jpg', title: 'Story 8', link: 'story8.html' },
-      { image: 'story9.jpg', title: 'Story 9', link: 'story9.html' },
-      { image: 'story10.jpg', title: 'Story 10', link: 'story10.html' },
-      { image: 'story11.jpg', title: 'Story 11', link: 'story11.html' },
-      { image: 'story12.jpg', title: 'Story 12', link: 'story12.html' }
-      // Add more stories for Level 2 if needed
-    ],
-    // Levels 21 to 30
-    [
-      // Add stories for Level 21 to 30
-    ],
-    // Levels 31 to 40
-    [
-      // Add stories for Level 31 to 40
-    ],
-    // Levels 41 to 50
-    [
-      // Add stories for Level 41 to 50
-    ],
-    // Levels 51 to 60
-    [
-      // Add stories for Level 51 to 60
-    ]
-  ];
   
-  // Function to generate stories for a specific level
-  function generateLevelStories(level, stories) {
-    var storyList = document.querySelector('.story-list.level' + level);
-  
-    stories.forEach(function(story) {
-      var storyItem = document.createElement('li');
-      var storyLink = document.createElement('a');
-      var storyImage = document.createElement('img');
-      var storyTitle = document.createElement('p');
-  
-      storyLink.href = story.link;
-      storyLink.textContent = story.title;
-  
-      storyImage.src = 'images/' + story.image;
-      storyImage.alt = story.title;
-  
-      storyItem.appendChild(storyImage);
-      storyItem.appendChild(storyTitle);
-      storyTitle.appendChild(storyLink);
-  
-      storyList.appendChild(storyItem);
-    });
-  }
-  
-  // Generate stories for each level
-  for (var i = 0; i < levelsData.length; i++) {
-    generateLevelStories(i + 1, levelsData[i]);
-  }
-*/
 
-  
+
+
+
+
+
+
+
+
+
+
+
+
